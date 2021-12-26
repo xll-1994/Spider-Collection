764f2be0a9e51de98eb5958cd1ee3f7bd8295348
flip-the-earth <359211258@qq.com>
2021-12-26 12:33:54 +0800

    新增导出为xls的模式

--- ZhiHu-Spider/util/save_data.py
+++ ZhiHu-Spider/util/save_data.py
@@ -6,6 +6,10 @@
 
 from database import MysqlClient
 from database import MongoDBClient
+import xlwt
+import xlrd
+from xlutils.copy import copy
+import json
 
 
 class SaveData(object):
@@ -21,3 +25,23 @@ class SaveData(object):
         mongo_client = MongoDBClient()
         db = mongo_client.db
         db[table_name].update_one({unique_id: self.data[unique_id]}, {'$set': self.data}, upsert=True)
+
+    def to_xls(self, table_name):
+        file_name = 'data/{}.xls'.format(table_name)
+        try:
+            rd_book = xlrd.open_workbook(file_name)
+            rd_sheet = rd_book.sheet_by_name('sheet')
+            sheet_rows = rd_sheet.nrows
+            wt_book = copy(rd_book)
+            wt_sheet = wt_book.get_sheet('sheet')
+        except:
+            wt_book = xlwt.Workbook(encoding='utf-8')
+            wt_sheet = wt_book.add_sheet('sheet')
+            my_keys = list(self.data.keys())
+            for col in range(len(my_keys)):
+                wt_sheet.write(0, col, my_keys[col])
+            sheet_rows = 1
+        my_values = list(self.data.values())
+        for col in range(len(my_values)):
+            wt_sheet.write(sheet_rows, col, my_values[col])
+        wt_book.save(file_name)
